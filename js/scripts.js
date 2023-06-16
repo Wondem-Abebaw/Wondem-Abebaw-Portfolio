ad8e9b03d8628effe6da8fdf995df7f63ea5f9d1
Wondem-Abebaw <128637148+Wondem-Abebaw@users.noreply.github.com>
2023-06-17 00:08:23 +0300

    code updated

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Header Toggle
+console.log("connected");
+let MenuBtn = document.getElementById("MenuBtn");
+MenuBtn.addEventListener("click", function (e) {
+  document.querySelector("body").classList.toggle("mobile-nav-active");
+  this.classList.toggle("fa-xmark");
+});
